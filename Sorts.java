63beee2fc99365bdb807344df927c8922ef630d1
Alex Conkey <aconkey20@stuy.edu>
2020-12-18 08:49:10 -0500

    set up while and for loop

---
+++ Sorts.java
@@ -0,0 +1,14 @@
+public class Sorts{
+  /**Bubble sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  int swaps = 1;
+  public static void bubbleSort(int[] data){
+    while (swaps != 0){
+      for(int i = 0; i < data.length-1; i++){
+
+      }
+    }
+  }
+}
